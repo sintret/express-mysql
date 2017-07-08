66c5823ea2a37296509af2fc304bf80f5c5a8cce
andy fitria <sintret@gmail.com>
2017-07-08 23:47:15 +0700

    f

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
 var express = require("express");
 var app = express();
 var path = require("path");
-//const Sequelize = require('sequelize');
-/*
- const sequelize = new Sequelize("test","root","",{
- host:'localhost',
- dialect:'mysql'
- });
- */
+var passport = require('passport');
+var Strategy = require('passport-local').Strategy;
 
-var sequelize = require("./models/config");
+// Use application-level middleware for common functionality, including
+// logging, parsing, and session handling.
+app.use(require('morgan')('combined'));
+app.use(require('cookie-parser')());
+app.use(require('body-parser').urlencoded({extended: true}));
+app.use(require('express-session')({secret: 'keyboard cat', resave: false, saveUninitialized: false}));
+
 var Info = require("./models/Info.js");
+var User = require("./models/User.js");
 
 // set the view engine to ejs
 //create folder views
@@ -29,7 +31,7 @@ app.use('/bower_components', express.static(path.join(__dirname, 'bower_componen
 app.get("/", function (req, res) {
     Info.findById(1)
         .then(function (info) {
-            res.render('pages/index', {data: info.dataValues, page_name: ""});
+            res.render('pages/index', {data: info.dataValues, page_name: "",session: req.user});
         });
     /*Info.create({
      title: "Express Js with mysql",
@@ -45,18 +47,76 @@ app.get("/about", function (req, res) {
     var About = require("./models/about.js");
     About.findById(1).then(function (model) {
         console.log(model.dataValues);
-        res.render('pages/about', {data: model.dataValues, page_name: "about"});
+        res.render('pages/about', {data: model.dataValues, page_name: "about",session: req.user});
     });
 });
 
-app.get("/login", function (req, res) {
-    res.render('pages/login', {data: {page_name: "login"}});
-    // res.render('pages/login', {page_name: "login"});
-});
-app.post("/login", function (req, res) {
-    res.render('pages/login', {page_name: "login"});
+passport.use(new Strategy(
+    function (username, password, cb) {
+        User.findOne({
+            where: {username: username}
+        }).then(function (user) {
+            if (!user) {
+                return cb(null, false);
+            }
+            if (user.password != password) {
+                return cb(null, false);
+            }
+            return cb(null, user);
+        });
+    }));
+
+passport.serializeUser(function (user, cb) {
+    cb(null, user.id);
 });
 
+passport.deserializeUser(function (id, cb) {
+    User.findById(id).then(function (user) {
+        if (!user) {
+            return cb(err);
+        }
+        cb(null, user);
+    });
+});
+
+// Initialize Passport and restore authentication state, if any, from the
+// session.
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.get("/login", function (req, res) {
+    res.render('pages/login', {data: {page_name: "login"}});
+});
+
+app.post('/login',
+    passport.authenticate('local', {failureRedirect: '/login'}),
+    function (req, res) {
+        res.redirect('/profile');
+    });
+
+app.get('/profile', 
+    require('connect-ensure-login').ensureLoggedIn(),
+    function(req, res){
+        console.log(req);
+        res.render('pages/profile', { user: req.user,session: req.user });
+    });
+
+
+app.get('/logout',
+    function(req, res){
+        req.logout();
+        res.redirect('/');
+    });
+
+function checkLogin(req, res){
+    if(req.user){
+        req.next();     //If session exists, proceed to page
+    } else {
+        var err = new Error("Not logged in!");
+        console.log(req.user);
+        req.next(err);  //Error, trying to access unauthorized page!
+    }
+}
 
 app.listen(3000, function () {
     console.log("express running on port 3000" + path.join(__dirname, "static"));
--- models/User.js
+++ models/User.js
@@ -1,3 +1,24 @@
 /**
  * Created by ASUS-PC on 7/8/2017.
  */
+
+const Sequelize = require('sequelize');
+var sequelize = require('./config.js');
+
+var User = sequelize.define('User', {
+    username: {
+        type: Sequelize.STRING,
+        unique: true
+    },
+    password: {
+        type: Sequelize.STRING
+    },
+    fullname: {
+        type: Sequelize.STRING
+    },
+    email: {
+        type: Sequelize.STRING
+    }
+});
+
+module.exports = User;
